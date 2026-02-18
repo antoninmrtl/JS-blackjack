08e062268efff163291491c0d9826f6c762e86be
antonin martel <antonin.mrtl.pr@gmail.com>
2026-02-18 21:10:41 +0100

    Add button NEW CARD

--- js/index.js
+++ js/index.js
@@ -1,12 +1,35 @@
-let firstCard = 10
+let firstCard = 6
 let secondCard = 11
+let thirdCard;
 let sum = firstCard + secondCard
 let hasBJ = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById('message-el')
+let sumEl = document.getElementById('sum-el')
+let cardEl= document.getElementById('card-el')
+
+
+
 const buttonStart = document.getElementById('start')
-buttonStart.addEventListener('click', function(){
-    console.log(message)
+buttonStart.addEventListener('click', startGame)
+
+function startGame(){
+    messageEl.innerText= message;
+    sumEl.innerText = `Sum: ${sum}`
+    if(thirdCard === undefined){
+        cardEl.innerText = `Cards: ${firstCard}  ${secondCard}`
+    }else {
+        cardEl.innerText = `Cards: ${firstCard}  ${secondCard}  ${thirdCard}`
+    }
+
+}
+
+const buttonCard = document.getElementById('newCard')
+buttonCard.addEventListener('click', function(){
+    thirdCard = 2
+    sum += thirdCard
+    startGame()
 })
 
 if(sum <= 20){
